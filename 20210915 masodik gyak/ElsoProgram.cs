fadd6fc94bff083af487b044bca8532040dcdb1d
veres9 <veres9@IIT136>
2021-09-15 10:32:06 +0200

    Add project files.

---
+++ 20210915 masodik gyak/ElsoProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _20210915_masodik_gyak
+{
+    class ElsoProgram
+
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Számológép:");
+            Console.WriteLine(3 + " + " + 2 + " = " + (3 + 2));
+            Console.WriteLine("3 + 2 = " + (3 + 2));
+            Console.WriteLine("3 + 2 = {0}", 3 + 2);
+            Console.WriteLine($"3 + 2 = {3 + 2}");
+            Console.WriteLine("3 + 2 = {0}", "öt");
+            Console.ReadKey();
+
+            /// komment
+            //  komment
+            /* komment */
+            
+        }
+    }
+}
