18cb8359ec73383d87a366cce62df3a07f7123c3
Blady97 <cs.veres.alex@gmail.com>
2021-09-15 11:05:50 +0200

    Raktam egy üres sort a 12. sorba is.

--- 20210915 masodik gyak/ElsoProgram.cs
+++ 20210915 masodik gyak/ElsoProgram.cs
@@ -9,6 +9,7 @@ namespace _20210915_masodik_gyak
         {
 
             Console.WriteLine("Számológép:");
+
             Console.WriteLine(3 + " + " + 2 + " = " + (3 + 2));
             Console.WriteLine("3 + 2 = " + (3 + 2));
             Console.WriteLine("3 + 2 = {0}", 3 + 2);
